db81e78ce4055be731fb3792c8e1cbd2cb1b26de
nequxwacs <dtabyldiev2006@gmail.com>
2023-03-14 14:24:57 +0600

    new file:   exam_app/public/img/Review 36.jpg 	new file:   exam_app/public/img/Review 37.jpg 	new file:   exam_app/public/img/Review 38.jpg 	modified:   exam_app/src/components/Card/Review/Review.jsx 	modified:   exam_app/src/components/Card/Review/Review.module.css 	new file:   exam_app/src/components/Card/Review/google_logo.svg 	new file:   exam_app/src/components/Card/Review/star.svg

--- exam_app/src/App.jsx
+++ exam_app/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import ShowFlight from "./components/Card/Flight/ShowFlight/ShowFlight";
+import Flight from "../public/img/Flight.jpg";
+import Hotels from "../public/img/Hotels.jpg";
+import Review from "./components/Card/Review/Review";
+import mountains from "../public/img/Review 36.jpg";
+import northern_lights from "../public/img/Review 37.jpg";
+import ice_floes from "../public/img/Review 38.jpg";
 
 function App() {
   return <div className="App"></div>;
