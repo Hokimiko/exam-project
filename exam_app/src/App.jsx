336adf12388cfc22725e35f1394fb458c0836658
Hokimiko <abubekerovkhakim@gmail.com>
2023-03-14 14:34:39 +0600

    modified:   exam_app/src/App.jsx 	modified:   exam_app/src/components/Card/Flight/ShowFlight/ShowFlight.jsx 	deleted:    exam_app/src/components/Card/Flight/ShowFlight/Vector.svg

--- exam_app/src/App.jsx
+++ exam_app/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import FlightsSelection from "./components/Card/Flight/flightsSelection/flightsSelection";
+import ShowFlight from "./components/Card/Flight/ShowFlight/ShowFlight";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <FlightsSelection/>
+    <ShowFlight/>
+  </div>;
 }
 
 export default App;
--- exam_app/src/components/Card/Flight/ShowFlight/ShowFlight.jsx
+++ exam_app/src/components/Card/Flight/ShowFlight/ShowFlight.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./ShowFlight.module.css";
-import vector from "./Vector.svg";
+
 
 function ShowFlight(props) {
   return (
