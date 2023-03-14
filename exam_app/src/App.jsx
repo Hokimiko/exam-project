4e9d26724ae84135aa6eac2381fabe01eae8ce77
nequxwacs <dtabyldiev2006@gmail.com>
2023-03-14 16:02:34 +0600

    Changes to be committed: 	new file:   exam_app/src/Components/Navbar/Logo.svg 	new file:   exam_app/src/Components/Navbar/Navbar.jsx 	new file:   exam_app/src/Components/Navbar/Navbar.module.css 	new file:   exam_app/src/Components/Navbar/main.jpg

--- exam_app/src/App.jsx
+++ exam_app/src/App.jsx
@@ -1,16 +1,8 @@
-
 import { useState } from "react";
 import "./App.css";
-import ShowFlight from "./components/Card/Flight/ShowFlight/ShowFlight";
 
 function App() {
-
-  return (
-    <div className="App">
-
-    </div>
-    
-  )
+  return <div className="App"></div>;
 }
 
 export default App;
