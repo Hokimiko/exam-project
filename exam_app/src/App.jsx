a1449536a9a042e359de2ad4d2bc2b806c43f444
Hokimiko <abubekerovkhakim@gmail.com>
2023-03-15 16:06:20 +0600

    modified:   exam_app/src/App.jsx

--- exam_app/src/App.jsx
+++ exam_app/src/App.jsx
@@ -1,38 +1,10 @@
-
-
-import { useState } from 'react'
-import './App.css'
-import CardFooter from './Components/Card.footer/CardFooter'
-import Ticket from './Components/Card/Flight/Ticket/Ticket'
-import Footer from './Components/Footer/Footer'
-
 import { useState } from "react";
 import "./App.css";
-import FlightsSelection from "./components/Card/Flight/flightsSelection/flightsSelection";
-import ShowFlight from "./components/Card/Flight/ShowFlight/ShowFlight";
-import Review from "./components/Card/Review/Review";
-import CardFooter from "./components/CardFooter/CardFooter";
-import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="navbar">
-
-        </div>
-        <div className="flights_stays">
-          <FlightsSelection/>
-        </div>
-      </header>
-      <main>
-
-      </main>
-      <footer>
-        <div className="footer">
-          <Footer/>
-        </div>
-      </footer>
+      <h1>bslfmk</h1> 
     </div>
   );
 }
