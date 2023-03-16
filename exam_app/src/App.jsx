30adbe5d1fcc7696c16117a8377e7453908de76e
Namazbek <talantbekovnamazbek8@gmail.com>
2023-03-16 17:11:16 +0300

    Changes to be committed: 	new file:   exam_app/public/img/Rectangle 3 (5).png 	new file:   exam_app/public/img/Rectangle 3 (6).png 	new file:   exam_app/public/img/Rectangle 3 (7).png 	new file:   exam_app/public/img/Rectangle 3 (8).png 	new file:   exam_app/src/Components/Card/Flight/BookFlight/BookGrid.jsx 	new file:   exam_app/src/Components/Card/Flight/BookFlight/BookGrid.module.css

--- exam_app/src/App.jsx
+++ exam_app/src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import './App.css'
 import CardFooter from './Components/Card.footer/CardFooter'
+import BookGrid from './Components/Card/Flight/BookFlight/BookGrid'
 import Ticket from './Components/Card/Flight/Ticket/Ticket'
 import Footer from './Components/Footer/Footer'
 
@@ -9,8 +10,7 @@ function App() {
 
   return (
     <div className="App">
-            <Ticket img="./public/img/turkey.png" city='Istanbul, Turkey' />
-            <Ticket img="./public/img/turkey.png" city='Istanbul, Turkey' />
+            <BookGrid/>
     </div>
     
   )
